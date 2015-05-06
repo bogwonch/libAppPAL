c06405291c718db2ba3db717c707b9d78c2d153e
Joseph Hallett <bogwonch@bogwonch.net>
2015-05-06 15:26:25 +0100

    Build a JAR

--- Makefile
+++ Makefile
@@ -9,27 +9,33 @@ CLASSPATH=lib/antlr-runtime-4.5.jar:.
 # Java dependencies must be checked in to be built
 SRC=$(shell git ls-files \*.java)
 
-GRAMMAR= apppal/logic/grammar/AppPAL.g4
-PARSERSRC= apppal/logic/grammar/AppPALBaseListener.java \
+GRAMMAR=apppal/logic/grammar/AppPAL.g4
+PARSER_SRC=apppal/logic/grammar/AppPALBaseListener.java \
 					 apppal/logic/grammar/AppPALBaseVisitor.java  \
 					 apppal/logic/grammar/AppPALLexer.java        \
 					 apppal/logic/grammar/AppPALListener.java     \
 					 apppal/logic/grammar/AppPALParser.java       \
 					 apppal/logic/grammar/AppPALVisitor.java
-
+MANIFEST=Manifest.txt
+TARGET_JAR=AppPAL.jar
 
 # WARNING: use GNU make or build each step in all in order ;-)
-all: | parser classes
+all: | parser classes jar
+
+jar: $(TARGET_JAR)
 
 # Compile each Java source file
-classes: $(PARSERSRC:.java=.class) $(SRC:.java=.class) 
+classes: $(PARSER_SRC:.java=.class) $(SRC:.java=.class) 
 
 # Build the parser files
 parser: $(PARSERSRC)
 
-$(PARSERSRC): $(GRAMMAR)
+$(PARSER_SRC): $(GRAMMAR)
 	$(ANTLR4) -visitor -package apppal.logic.grammar $<
 
+$(TARGET_JAR): classes $(MANIFEST)
+	jar cfm $(@) $(MANIFEST) apppal lib
+
 clean:
-	$(RM) $(shell find -name \*.class) $(PARSERSRC)
+	$(RM) $(shell find -name \*.class) $(PARSER_SRC)
 
---
+++ apppal/logic/grammar/AppPALVisitor.java
@@ -0,0 +1,143 @@
+// Generated from apppal/logic/grammar/AppPAL.g4 by ANTLR 4.5
+package apppal.logic.grammar;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link AppPALParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface AppPALVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by the {@code variable}
+	 * labeled alternative in {@link AppPALParser#e}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariable(AppPALParser.VariableContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code constant}
+	 * labeled alternative in {@link AppPALParser#e}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitConstant(AppPALParser.ConstantContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code zero}
+	 * labeled alternative in {@link AppPALParser#d}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitZero(AppPALParser.ZeroContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code inf}
+	 * labeled alternative in {@link AppPALParser#d}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitInf(AppPALParser.InfContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code predicate}
+	 * labeled alternative in {@link AppPALParser#vp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPredicate(AppPALParser.PredicateContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code canSay}
+	 * labeled alternative in {@link AppPALParser#vp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCanSay(AppPALParser.CanSayContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code canActAs}
+	 * labeled alternative in {@link AppPALParser#vp}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCanActAs(AppPALParser.CanActAsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link AppPALParser#fact}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFact(AppPALParser.FactContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link AppPALParser#claim}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitClaim(AppPALParser.ClaimContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link AppPALParser#assertion}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssertion(AppPALParser.AssertionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link AppPALParser#ac}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAc(AppPALParser.AcContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code entity}
+	 * labeled alternative in {@link AppPALParser#ce}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEntity(AppPALParser.EntityContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code function}
+	 * labeled alternative in {@link AppPALParser#ce}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunction(AppPALParser.FunctionContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code true}
+	 * labeled alternative in {@link AppPALParser#ce}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTrue(AppPALParser.TrueContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code false}
+	 * labeled alternative in {@link AppPALParser#ce}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFalse(AppPALParser.FalseContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code negation}
+	 * labeled alternative in {@link AppPALParser#c}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNegation(AppPALParser.NegationContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code satisfaction}
+	 * labeled alternative in {@link AppPALParser#c}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSatisfaction(AppPALParser.SatisfactionContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code equality}
+	 * labeled alternative in {@link AppPALParser#c}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEquality(AppPALParser.EqualityContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code conjugation}
+	 * labeled alternative in {@link AppPALParser#c}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitConjugation(AppPALParser.ConjugationContext ctx);
+}
