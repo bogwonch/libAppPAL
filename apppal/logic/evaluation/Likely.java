3dad33af0b5416c867a236239fbd8212b05ac7b5
Joseph Hallett <bogwonch@bogwonch.net>
2015-08-14 11:33:26 +0100

    Fixed bug in likely relating to can-act-as and can-say likeliness

--- apppal/logic/evaluation/Likely.java
+++ apppal/logic/evaluation/Likely.java
@@ -28,61 +28,74 @@ public class Likely
     {
       if (a.isCanActAs())
       {
-        E subject = a.says.consequent.subject;
-        E renaming = ((CanActAs) a.says.consequent.object).renaming;
+        final E subject = a.says.consequent.subject;
+        final E renaming = ((CanActAs) a.says.consequent.object).renaming;
         if (renaming.kind == EKind.VARIABLE)
           canActAs_variables.add(subject);
         else
-        {
-          if (canActAs_mapables.containsKey(subject))
-          {
-            final Set<E> set = canActAs_mapables.get(subject);
-            set.add(renaming);
-            canActAs_mapables.put(subject, set);
-          }
-          else
-          {
-            final Set<E> set = new HashSet<>();
-            set.add(renaming);
-            canActAs_mapables.put(subject, set);
-          }
-        }
+          add_mapable(canActAs_mapables, subject, renaming);
       }
       else if (a.isCanSay())
       {
-        E delegated = a.says.consequent.subject;
-        E speaker = a.speaker;
+        final E delegated = a.says.consequent.subject;
+        final E speaker = a.speaker;
         if (delegated.kind == EKind.VARIABLE)
           canSay_variables.add(speaker);
         else
+          add_mapable(canSay_mapables, speaker, delegated);
+      }
+    }
+
+    // Fixup can-say and can-act-as together mapables
+    for (E e : canActAs_variables)
+      for (E k : canSay_mapables.keySet())
+      {
+        Set<E> ms = canSay_mapables.get(k);
+        if (ms.contains(e))
+          canSay_variables.add(k);
+      }
+    for (E caK : canActAs_mapables.keySet())
+    {
+      Set<E> caMs = canActAs_mapables.get(caK);
+      for(E caM : caMs)
+      {
+        for(E csK : canSay_mapables.keySet())
         {
-          if (canSay_mapables.containsKey(speaker))
-          {
-            final Set<E> set = canSay_mapables.get(speaker);
-            set.add(delegated);
-            canSay_mapables.put(speaker, set);
-          }
-          else
-          {
-            final Set<E> set = new HashSet<>();
-            set.add(delegated);
-            canSay_mapables.put(speaker, set);
-          }
+          Set<E> csMs = canSay_mapables.get(csK);
+          if (csMs.contains(caM))
+            add_mapable(canSay_mapables, csK, caK);
         }
       }
     }
 
+    /* // Copious amounts of debugging info */
     /* System.err.println("[!] Likely"); */
     /* System.err.println("    CA Vars:"); */
     /* for (E e : canActAs_variables) System.err.println("      "+e); */
     /* System.err.println("    CA Mapped:"); */
     /* for (E e : canActAs_mapables.keySet()) */
-      /* for (E ee : canActAs_mapables.get(e)) */
-        /* System.err.println("      "+e+" -> "+ee); */
-
+    /*   for (E ee : canActAs_mapables.get(e)) */
+    /*     System.err.println("      "+e+" -> "+ee); */
     /* for (E e : canSay_mapables.keySet()) */
     /*   for (E ee : canSay_mapables.get(e)) */
     /*     System.err.println("[!] "+e+" |--> "+ee); */
+
+  }
+
+  public void add_mapable(Map<E, Set<E>> map, E key, E value)
+  {
+    if (map.containsKey(key))
+    {
+      final Set<E> set = map.get(key);
+      set.add(value);
+      map.put(key, set);
+    }
+    else
+    {
+      final Set<E> set = new HashSet<>();
+      set.add(value);
+      map.put(key, set);
+    }
   }
 
   public boolean canSay(final E speaker, final E delegated)
