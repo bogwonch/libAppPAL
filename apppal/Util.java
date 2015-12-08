60bcad836f8ea731e31ddc547f8ba9359345f87b
Bogwonch <bogwonch@bogwonch.net>
2015-12-08 18:27:46 +0000

    skeleton implementation (does nothing)

--- apppal/Util.java
+++ apppal/Util.java
@@ -2,18 +2,20 @@ package apppal;
 
 public class Util
 {
+    public static boolean enable_debug = false;
+
     public static void info(String msg)
-    {   System.err.println("\033[0;32m[I]\033[0;39m]: "+msg); }
+    {   System.err.println("\033[0;32m[I]\033[0;39m: "+msg); }
 
     public static void debug(String msg)
-    {   System.err.println("\033[0;34m[D]\033[0;39m]: "+msg); }
+    {   if (Util.enable_debug) System.err.println("\033[0;34m[D]\033[0;39m: "+msg); }
 
     public static void warn(String msg)
-    {   System.err.println("\033[0;33m[W]\033[0;39m]: "+msg); }
+    {   System.err.println("\033[0;33m[W]\033[0;39m: "+msg); }
 
     public static void error(String msg)
     {
-        System.err.println("\033[0;31m[E]\033[0;39m]: "+msg);
+        System.err.println("\033[0;31m[E]\033[0;39m: "+msg);
         System.exit(1);
     }
 }
--- apppal/application/Lint.java
+++ apppal/application/Lint.java
@@ -1,18 +1,77 @@
 package apppal.application;
 
 import apppal.Util;
-
-import java.util.List;
+import apppal.logic.evaluation.AC;
+import apppal.logic.language.Assertion;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Lint
 {
-    private boolean check_completeness = false;
-    private boolean check_consistency = false;
-    private boolean check_redundancy = false;
+    private final boolean check_completeness;
+    private final boolean check_consistency;
+    private final boolean check_redundancy;
 
-    public Lint()
+    private final List<String> policies;
+    private final AC ac;
+
+    public Lint(final boolean completeness,
+                final boolean consistency,
+                final boolean redundancy,
+                final List<String> policies)
     {
+        this.check_completeness = completeness;
+        this.check_consistency  = consistency;
+        this.check_redundancy   = redundancy;
+        this.policies = policies;
+        this.ac = new AC();
+
+        if (!(check_completeness ||check_consistency || check_redundancy))
+            Util.warn("no checks requested");
+
+        if (policies.size() == 0)
+            Util.warn("no files to check");
+
+        this.load_policies();
+
+        if (this.check_completeness)
+        {
+            Util.warn("completeness checks are unimplemented");
+        }
+        if (this.check_consistency)
+        {
+            Util.warn("consistency checks are unimplemented");
+        }
+        if (this.check_redundancy)
+        {
+            Util.warn("redundancy checks are unimplemented");
+        }
+    }
+
+    private void load_policies()
+    {
+        int count = 0;
+        for (final String file : this.policies)
+        {
+            Util.debug("loading '"+file+"'");
+            try
+            {
+                final int old_size = this.ac.assertions.size();
+                this.ac.merge(new AC(new FileInputStream(file)));
+                final int new_size = this.ac.assertions.size();
+
+                Util.debug("loaded "+(new_size-old_size)+" assertions ("+new_size+" in total)");
+                count += 1;
+            }
+            catch (IOException e)
+            {
+                Util.error("failed to load '"+file+"'");
+            }
+        }
+
+        Util.info("loaded "+count+"/"+this.policies.size()+" files of "+this.ac.assertions.size()+" assertions");
     }
 
     public static void main(final String[] args)
@@ -49,6 +108,10 @@ public class Lint
                 redundancy = true;
                 break;
 
+            case "--debug":
+                Util.enable_debug = true;
+                break;
+
             default:
                 if (arg.startsWith("-"))
                     Util.warn("unrecognised flag: '"+arg+"': ignoring");
@@ -57,8 +120,9 @@ public class Lint
                 break;
             }
         }
-    }
 
+        final Lint program = new Lint(completeness, consistency, redundancy, policies);
+    }
 
     public static void main_help()
     {
@@ -72,5 +136,6 @@ public class Lint
         System.out.println("  --consistency:  check policies for consistency");
         System.out.println("  --redundancy:   check policies for redundancy");
         System.out.println("  --all:          check policies for all of the above");
+        System.out.println("  --debug:        enable debugging messages");
     }
 }
