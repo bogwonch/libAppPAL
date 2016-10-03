678783baf360612d01650fee7d3489106e11884b
Joseph Hallett <josephhallett@gmail.com>
2016-10-03 10:45:04 +0100

    Take advantage of Pragmata's ligatures

--- apppal/Util.java
+++ apppal/Util.java
@@ -5,17 +5,17 @@ public class Util
     public static boolean enable_debug = false;
 
     public static void info(String msg)
-    {   System.err.println("\033[0;32m[I]\033[0;39m: "+msg); }
+    {   System.err.println("\033[0;32m[INFO]\033[0;39m "+msg); }
 
     public static void debug(String msg)
-    {   if (Util.enable_debug) System.err.println("\033[0;34m[D]\033[0;39m: "+msg); }
+    {   if (Util.enable_debug) System.err.println("\033[0;34m[DEBUG]\033[0;39m "+msg); }
 
     public static void warn(String msg)
-    {   System.err.println("\033[0;33m[W]\033[0;39m: "+msg); }
+    {   System.err.println("\033[0;33m[WARNING]\033[0;39m "+msg); }
 
     public static void error(String msg)
     {
-        System.err.println("\033[0;31m[E]\033[0;39m: "+msg);
+        System.err.println("\033[0;31m[ERROR]\033[0;39m "+msg);
         System.exit(1);
     }
 }
